0927109d1861807dcf1fd0f9895b89a5619cda85
MigsGlez <98905548+MigsGlez@users.noreply.github.com>
2022-09-21 14:18:11 -0700

    Add files via upload

---
+++ TDArrays.java
@@ -0,0 +1,12 @@
+public class TDArrays {
+
+    
+    public static void main(String[] args) {
+
+        int[][] grades = new int [3][4];
+        System.out.println("\tHarry: " + Arrays.toString(grades[0]));
+        System.out.println("\tRon: " + Arrays.toString(grades[1]));
+        System.out.println("\tHermione: " + Arrays.toString(grades[2]));
+    }
+    
+}
